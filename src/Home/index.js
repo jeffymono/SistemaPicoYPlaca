604139af37c005a6769a10e49b596b7219f97397
jeffymono <jeffymona123@gmail.com>
2020-09-20 20:29:14 -0500

    Arreglo de errores datos de scan

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,63 +1,61 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
-import { Feather } from '@expo/vector-icons'; 
+import { Feather } from "@expo/vector-icons";
 
 const MESES = {
-  enero: { position: 0, value: 1 },
-  febrero: { position: 1, value: 2 },
-  marzo: { position: 2, value: 3 },
-  abril: { position: 3, value: 4 },
-  mayo: { position: 4, value: 5 },
-  junio: { position: 5, value: 6 },
-  julio: { position: 6, value: 7 },
-  agosto: { position: 7, value: 8 },
-  septiembre: { position: 8, value: 9 },
-  octubre: { position: 9, value: 10 },
-  noviembre: { position: 10, value: 11 },
-  diciembre: { position: 11, value: 12 },
+  enero: { value: 1 },
+  febrero: { value: 2 },
+  marzo: { value: 3 },
+  abril: { value: 4 },
+  mayo: { value: 5 },
+  junio: { value: 6 },
+  julio: { value: 7 },
+  agosto: { value: 8 },
+  septiembre: { value: 9 },
+  octubre: { value: 10 },
+  noviembre: { value: 11 },
+  diciembre: { value: 12 },
 };
 
-export default function Home({dataScan=null}={}) {
-  
-  const [placa, setPlaca] = useState("");
+export default function Home({ dataScan }) {
+  const [placa, setPlaca] = useState(dataScan ? dataScan : "");
   const [showError, setShowError] = useState(false);
-
 
   const handleOnChange = (text) => {
     setPlaca(text);
   };
-  const placaParaUsar = dataScan ? dataScan : placa
 
-  useEffect(()=>{
-  if(placaParaUsar.trim() !== ""){
+  useEffect(() => {
+    if (placa.trim() !== "") {
       setShowError(false);
     }
-  },[placaParaUsar,setShowError])
+  }, [placa, setShowError]);
 
   const handleOnPress = useCallback(() => {
-
     const date = new Date();
     const arregloMeses = Object.values(MESES);
-    const mes = arregloMeses[date.getMonth()].value;
+    const mes = arregloMeses[date.getMonth()];
+
     const message1 = "Circula los días: Lunes, Miércoles, Viernes y Domingo";
     const message2 = "Circula los días: Martes, Jueves, Sábado y Domingo";
+
     const placaValida =
-    placaParaUsar.length === 6 ? placaParaUsar.length === 6 : placaParaUsar.length === 7;
-    const ultimoDigito = parseInt(placaParaUsar.slice(6, 7))
-      ? parseInt(placaParaUsar.slice(6, 7))
-      : parseInt(placaParaUsar.slice(4, 5));
+      placa.length === 6 ? placa.length === 6 : placa.length === 7;
+
+    const ultimoDigito = parseInt(placa.slice(6, 7))
+      ? parseInt(placa.slice(6, 7))
+      : parseInt(placa.slice(4, 5));
+
     const placaValidacionCarro = /^[A-Z]{3}[0-9]{4}$/;
     const placaValidacionMoto = /^[A-Z]{2}[0-9]{3}[A-Z]$/;
 
-    if (placaParaUsar.trim() === "") {
+    if (placa.trim() === "") {
       setShowError(true);
-    }
-    if (showError) {
       Alert.alert("Resultado", "Ingrese una placa");
-    } else if(!showError) {
+    } else if (!showError) {
       if (
-        placaValidacionCarro.test(placaParaUsar.toUpperCase()) ||
-        placaValidacionMoto.test(placaParaUsar.toUpperCase())
+        placaValidacionCarro.test(placa.toUpperCase()) ||
+        placaValidacionMoto.test(placa.toUpperCase())
       ) {
         if (mes % 2 === 0) {
           if (placaValida) {
@@ -65,8 +63,6 @@ export default function Home({dataScan=null}={}) {
               Alert.alert("Resultado", message1);
             } else if (ultimoDigito % 2 !== 0)
               Alert.alert("Resultado", message2);
-          } else {
-            Alert.alert("Resultado", "Placa inválida");
           }
         } else if (mes % 2 !== 0) {
           if (placaValida) {
@@ -74,27 +70,28 @@ export default function Home({dataScan=null}={}) {
               Alert.alert("Resultado", message2);
             } else if (ultimoDigito % 2 !== 0)
               Alert.alert("Resultado", message1);
-          } else {
-            Alert.alert("Resultado", "Placa inválida");
           }
         }
-      } else {
-        Alert.alert("Resultado", "Placa incorrecta");
       }
+    } else {
+      Alert.alert("Resultado", "Placa incorrecta");
     }
-  }, [setShowError, placaParaUsar]);
+  }, [setShowError, placa]);
 
-  useEffect(()=>{
-    if(dataScan) handleOnPress()
-  },[])
-  const errorInput = showError ? "red" :"#fff"
+  useEffect(() => {
+    if (dataScan) {
+      handleOnPress();
+    }
+  }, [dataScan]);
+
+  const errorInput = showError ? "red" : "#fff";
   return (
     <View
       style={{
         display: "flex",
         flexDirection: "column",
         width: "100%",
-        padding: 15
+        padding: 15,
       }}
     >
       <Text style={{ color: "#fff" }}>Ingresar el placa</Text>
@@ -105,11 +102,11 @@ export default function Home({dataScan=null}={}) {
           height: 50,
           marginTop: 15,
           marginBottom: 0,
-          borderRadius:5,
-          borderWidth:2,
-        borderColor:errorInput
+          borderRadius: 5,
+          borderWidth: 2,
+          borderColor: errorInput,
         }}
-        autoCapitalize='characters'
+        autoCapitalize="characters"
         maxLength={7}
         value={placa}
         onChangeText={handleOnChange}
@@ -125,21 +122,30 @@ export default function Home({dataScan=null}={}) {
           * Este campo es requerido
         </Text>
       )}
-      <View style={{ backgroundColor: "#117FB9", marginBottom:12, borderRadius:5}}>
+      <View
+        style={{
+          backgroundColor: "#117FB9",
+          marginBottom: 12,
+          borderRadius: 5,
+        }}
+      >
         <TouchableOpacity
           style={{
-            display:"flex",
-            flexDirection:"row",
-            justifyContent:"center",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
             alignItems: "center",
             backgroundColor: "#117FB9",
             padding: 0,
-            borderRadius:5
+            borderRadius: 5,
           }}
           onPress={handleOnPress}
         >
           <Feather name="search" size={24} color="white" />
-          <Text style={{ color: "#fff", marginBottom:15, marginTop:15 }}> BUSCAR </Text>
+          <Text style={{ color: "#fff", marginBottom: 15, marginTop: 15 }}>
+            {" "}
+            BUSCAR{" "}
+          </Text>
         </TouchableOpacity>
       </View>
       <View
@@ -147,7 +153,7 @@ export default function Home({dataScan=null}={}) {
           display: "flex",
           flexDirection: "row",
           alignItems: "center",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
       >
         <View
